872b2077b9933faf902d465916de079717d87bae
Leonardo-Pike-Excell <leonardopike.excell@gmail.com>
2025-01-10 17:25:12 +1100

    fix: use correct `bl_idname`
    Closes #8

--- source/keymaps.py
+++ source/keymaps.py
@@ -9,7 +9,7 @@ def register() -> None:
     assert bpy.context
     if kc := bpy.context.window_manager.keyconfigs.addon:
         km = kc.keymaps.new(name='Node Editor', space_type="NODE_EDITOR")
-        kmi = km.keymap_items.new("node.na_clear_locations", type='G', value='PRESS', alt=True)
+        kmi = km.keymap_items.new("node.na_recenter_selected", type='G', value='PRESS', alt=True)
         addon_keymaps.append((km, kmi))
 
 
